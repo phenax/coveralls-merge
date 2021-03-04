0060ca06c7721588d364f716c5bb898110daf0e0
Akshay Nair <phenax5@gmail.com>
2021-03-04 15:43:52 +0530

    Increases max buffer size for execSync calls to git

--- src/util/git.js
+++ src/util/git.js
@@ -4,24 +4,27 @@ const USERNAME_EMAIL_REGEX = /(.+)\s<(.+)>/,
     BRANCH_REGEX = /\* (.+)/,
     COMMIT_MESSAGE_REGEX = /\w+\s(.+)/;
 
+const exec = (cmd, optns = {}) =>
+    execSync(cmd, { maxBuffer: 100 * 1024 * 1024, ...optns }).toString();
+
 function getHeadId() {
-    return execSync('git rev-parse HEAD').toString().trim();
+    return exec('git rev-parse HEAD').trim();
 }
 
 function getCommitMessage() {
-    const message = execSync('git log -1 --pretty=%B --oneline').toString();
+    const message = exec('git log -1 --pretty=%B --oneline');
 
     return message.match(COMMIT_MESSAGE_REGEX)[1];
 }
 
 function getAuthorNameAndEmail() {
-    const gitShow = execSync('git show --format="%aN <%aE>" HEAD').toString();
+    const gitShow = exec('git show --format="%aN <%aE>" HEAD');
 
     return gitShow.match(USERNAME_EMAIL_REGEX).slice(1, 3);
 }
 
 function getCommitterNameAndEmail() {
-    const gitShow = execSync('git show --format="%cN <%cE>" HEAD').toString();
+    const gitShow = exec('git show --format="%cN <%cE>" HEAD');
 
     return gitShow.match(USERNAME_EMAIL_REGEX).slice(1, 3);
 }
@@ -41,13 +44,13 @@ function getHead() {
 }
 
 function getBranch() {
-    const branches = execSync('git branch').toString();
+    const branches = exec('git branch');
 
     return branches.match(BRANCH_REGEX)[1];
 }
 
 function getRemotes() {
-    const remotes = execSync('git remote -v').toString().split('\n');
+    const remotes = exec('git remote -v').split('\n');
 
     return remotes
         .filter(remote => remote.endsWith('(push)'))
